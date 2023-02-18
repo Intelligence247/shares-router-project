5165713941025011b7ce360f3f913920d72635cb
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-02-18 09:28:36 +0100

    cookies

--- src/Components/Cookies.jsx
+++ src/Components/Cookies.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {useState} from 'react'
-
+import "./cookies.css"
 const Cookies = () => {
     const [first, setfirst] = useState(true)
     const handleClick=()=>{
         setfirst(!first)
     }
   return (
-<div className="cookies fixed bottom-10">
+<div className="cookiesWrapper fixed bottom-10">
+  <div className={`cookies ${first?"active":''}`}>
+    
+  </div>
     <img src={first ? "/media/harmburger.svg" : "/media/harmburger2.svg"} className='w-[4rem]' onClick={handleClick} alt="" />
 </div>
   )
